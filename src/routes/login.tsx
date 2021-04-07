6e802d57a530067b96b6304a18410991890c676e
Valerii Brylov <valychbreak@gmail.com>
2021-04-07 21:04:57 +0200

    Add signin component with form

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,27 +1,5 @@
-import { useContext, useState } from "react";
-import { Redirect } from "react-router";
-import { useLocation } from "react-router-dom";
-import AuthenticationContext from "../components/authentication";
+import { Signin } from "../components/signin";
 
 export const Login = () => {
-
-    const authContext = useContext(AuthenticationContext)
-
-    const [redirectToReferrer, setRedirectToReferrer] = useState(false)
-    const { state } = useLocation<any>()
-
-    const handleLogin = () => {
-        authContext.authenticate({username: "test", password: "anothertest"}).then(_ => setRedirectToReferrer(true));
-    }
-
-    if (redirectToReferrer === true) {
-        return <Redirect to={state?.from || '/'} />
-    }
-
-    return (
-        <div>
-            <p>You must log in to view the page</p>
-            <button onClick={handleLogin}>Log in</button>
-        </div>
-    )
+    return <Signin />
 }
