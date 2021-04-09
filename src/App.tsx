a4bfda5e5b13b7bd6fa7df6329121e84b8c20eb5
Valerii Brylov <valychbreak@gmail.com>
2021-04-09 22:51:36 +0200

    [S17] Add app menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,74 @@
 
+import { makeStyles, Paper, Typography } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AuthenticatedRoute from "./components/authenticated-route";
 import { AuthenticationContextProvider } from "./components/authentication/authentication-provider";
+import AppMenu from "./components/header";
 import { ProfileBar } from "./components/profile";
 import BrowseProductsPage from "./pages/browse-products/BrowseProductsPage";
 import AddNewProduct from './pages/new-product/AddNewProductPage'
 import ScanTest from "./pages/scan-test/ScanTestPage";
 import { GroceriesTodo } from "./routes/groceries-todo";
 import { Login } from "./routes/login";
- 
+
+const useStyles = makeStyles((theme) => ({
+    root: {},
+    paper: {
+        marginBottom: theme.spacing(2)
+    },
+    table: {},
+    visuallyHidden: {
+        border: 0,
+        clip: "rect(0 0 0 0)",
+        height: 1,
+        margin: -1,
+        overflow: "hidden",
+        padding: 0,
+        position: "absolute"
+    },
+    container: {
+        maxHeight: 360
+    }
+}));
+
 export default function App() {
-  return (
-    <AuthenticationContextProvider>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/browse">Browse products</Link>
-              </li>
-              <li>
-                <Link to="/new-product">Add new item</Link>
-              </li>
-              <li>
-                <Link to="/groceries-todo">Groceries to buy</Link>
-              </li>
-              <li>
-                <Link to="/scan-test">Test scanner</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </ul>
-            <ProfileBar />
-          </nav>
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/browse">
-              <BrowseProductsPage />
-            </Route>
-            <Route path="/new-product">
-              <AddNewProduct />
-            </Route>
-            <AuthenticatedRoute path="/groceries-todo">
-              <GroceriesTodo />
-            </AuthenticatedRoute>
-            <Route path="/scan-test">
-              <ScanTest />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </AuthenticationContextProvider>
-  );
+    const classes = useStyles();
+
+    return (
+        <AuthenticationContextProvider>
+            <Router>
+                <div className={classes.root}>
+                    <Paper className={classes.paper}>
+                        <AppMenu />
+                        <ProfileBar />
+                        <Switch>
+                            <Route path="/browse">
+                              <BrowseProductsPage />
+                            </Route>
+                            <Route path="/new-product">
+                              <AddNewProduct />
+                            </Route>
+                            <AuthenticatedRoute path="/groceries-todo">
+                              <GroceriesTodo />
+                            </AuthenticatedRoute>
+                            <Route path="/scan-test">
+                              <ScanTest />
+                            </Route>
+                            <Route path="/login">
+                              <Login />
+                            </Route>
+                            <Route path="/">
+                              <Home />
+                            </Route>
+                        </Switch>
+                    </Paper>
+                </div>
+            </Router>
+        </AuthenticationContextProvider>
+    );
 }
- 
+
 function Home() {
-  return <h2>Home</h2>;
+    return <h2>Home</h2>;
 }
---
+++ src/components/header/menu.tsx
@@ -0,0 +1,111 @@
+import { FormControlLabel, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Switch, withStyles } from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import BorderInnerIcon from "@material-ui/icons/BorderInner";
+import SearchIcon from "@material-ui/icons/Search";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import React from "react"
+import { AccountBox, Home } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+const AppMenu = () => {
+
+    const [anchorElement, setAnchorElement] = React.useState(null);
+    const [mode, setMode] = React.useState(false);
+
+    const handleChangemode = (event: any) => {
+        setMode(event.target.checked);
+    };
+
+    const openMenu = (event: any) => {
+        setAnchorElement(event.currentTarget);
+    };
+
+    const closeMenu = () => {
+        setAnchorElement(null);
+    };
+
+    return (<>
+        <IconButton
+            aria-label="more"
+            aria-controls="long-menu"
+            aria-haspopup="true"
+            onClick={openMenu}
+        >
+            <MenuIcon />
+        </IconButton>
+
+        <StyledMenu
+            id="customized-menu"
+            anchorEl={anchorElement}
+            keepMounted
+            open={Boolean(anchorElement)}
+            onClose={closeMenu}
+        >
+            <MenuItem>
+                <FormControlLabel
+                    control={<Switch checked={mode} onChange={handleChangemode} />}
+                    label="Purchase mode"
+                />
+            </MenuItem>
+            <MenuItem component={Link} to='/' onClick={closeMenu}>
+                <ListItemIcon>
+                    <Home fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+            </MenuItem>
+            <MenuItem component={Link} to='/groceries-todo' onClick={closeMenu}>
+                <ListItemIcon>
+                    <AssignmentIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Groceries to buy" />
+            </MenuItem>
+            <MenuItem component={Link} to='/new-product' onClick={closeMenu}>
+                <ListItemIcon>
+                    <BorderInnerIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Add new product" />
+            </MenuItem>
+            <MenuItem component={Link} to='/browse' onClick={closeMenu}>
+                <ListItemIcon>
+                    <SearchIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Browse products" />
+            </MenuItem>
+            <MenuItem component={Link} to='/scan-test' onClick={closeMenu}>
+                <ListItemIcon>
+                    <VideocamIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Test scanner" />
+            </MenuItem>
+            <MenuItem component={Link} to='/login' onClick={closeMenu}>
+                <ListItemIcon>
+                    <AccountBox fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Login" />
+            </MenuItem>
+        </StyledMenu>
+    </>)
+}
+
+const StyledMenu = withStyles({
+    paper: {
+        border: "1px solid #d3d4d5"
+    }
+})((props: any) => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+        }}
+        transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+        }}
+        {...props}
+    />
+));
+
+export default AppMenu;
