0eb246d59cc31a5f76b936dde93183ef2dd10062
Valerii Brylov <valychbreak@gmail.com>
2021-04-11 01:07:13 +0200

    [S17] Replace react-select with material ui component

--- src/pages/groceries-todo/components/AddTodoItemForm.tsx
+++ src/pages/groceries-todo/components/AddTodoItemForm.tsx
@@ -1,13 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import Product from "../../../entity/Product";
-import AsyncCreatableSelect from "react-select/async-creatable";
-import { ActionMeta, OptionsType } from "react-select";
 import ProductApi from "../../../api/ProductApi";
 import TodoItemListContext, { TodoItemListContextType } from "../context/TodoItemListContext";
 import TodoItem from "../../../components/todo-item-list/types";
 import { RouteComponentProps, withRouter } from "react-router-dom";
-import { TableContainer, Table, TableHead, TableRow, TableCell, TextField, Button } from "@material-ui/core";
+import { TableContainer, Table, TableHead, TableRow, TableCell, TextField, Button, CircularProgress } from "@material-ui/core";
 import QuantityField from "../../../components/quantity-field";
+import { Autocomplete } from "@material-ui/lab";
 
 
 function ccyFormat(num: number) {
@@ -17,34 +16,46 @@ function ccyFormat(num: number) {
 }
 
 type ProductSelectItem = {
+    inputValue?: string;
     label: string,
     product: Product;
 }
 
 interface ProductSelectProps {
+    inputValue: string;
+    setInputValue(newValue: string): void;
     onProductSelect(selectedProduct: Product | null): void;
     onProductCreateOptionSelect(inputValue: string): void;
 }
 
-const ProductSelect = React.forwardRef<any, ProductSelectProps>((props: ProductSelectProps, ref: React.ForwardedRef<any>) => {
+const NEW_PRODUCT = new Product("", "", "");
 
-    const onItemTagValueChange = (value: string): Promise<OptionsType<ProductSelectItem>> => {
-        return new Promise((resolve, reject) => {
-            ProductApi.findMatchingBy(value)
-                .then(products => {
-                    let foundProducts: ProductSelectItem[] = products.map(product => { 
-                        return {label: product.productFullName as string, product: product}
-                    });
+const ProductSelect = (props: ProductSelectProps) => {
 
-                    resolve (foundProducts);
-                })
-        });
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = React.useState<ProductSelectItem[]>([]);
+    const loading = open && options.length === 0 && props.inputValue.length > 0;
+
+    const onInputChange = async (inputValue: string) => {
+        props.setInputValue(inputValue);
+        setOptions([]);
+        if (inputValue.length > 0) {
+            const loadedProducts = await ProductApi.findMatchingBy(inputValue);
+            let foundProducts: ProductSelectItem[] = loadedProducts.map(product => {
+                return { label: product.productFullName as string, product: product }
+            });
+            foundProducts.push({ inputValue: inputValue, label: `Add '${inputValue}' product`, product: NEW_PRODUCT });
+            setOptions(foundProducts);
+        }
     }
 
-    const onProductSelect = (selectedProduct: ProductSelectItem | OptionsType<ProductSelectItem> | null, action: ActionMeta<ProductSelectItem>) => {
+    const onProductSelect = (selectedProduct: ProductSelectItem | null) => {
         if (selectedProduct) {
-            // workaround: couldn't find a way how to resolve union types in this case
-            props.onProductSelect((selectedProduct as ProductSelectItem).product);
+            if (selectedProduct.product === NEW_PRODUCT && selectedProduct.inputValue) {
+                onProductOptionCreate(selectedProduct.inputValue);
+            } else {
+                props.onProductSelect((selectedProduct as ProductSelectItem).product);
+            }
         } else {
             props.onProductSelect(null);
         }
@@ -54,14 +65,39 @@ const ProductSelect = React.forwardRef<any, ProductSelectProps>((props: ProductS
         props.onProductCreateOptionSelect(inputValue);
     }
 
-    return (
-        <AsyncCreatableSelect loadOptions={onItemTagValueChange}
-                              onChange={onProductSelect}
-                              onCreateOption={onProductOptionCreate}
-                              isMulti={false}
-                              ref={ref}/>
-    )
-})
+    return (<>
+        <Autocomplete
+            open={open}
+            size="small"
+            inputValue={props.inputValue}
+            onOpen={() => setOpen(true)}
+            onClose={() => setOpen(false)}
+            options={options}
+            getOptionSelected={(option, selectedValue) => option.label === selectedValue.label}
+            getOptionLabel={(option) => option.label}
+            onChange={(event, newValue) => {
+                onProductSelect(newValue);
+            }}
+            onInputChange={(event, value) => {
+                onInputChange(value);
+            }}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Asynchronous"
+                    variant="standard"
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                    }} />
+            )} />
+    </>)
+};
 
 const DEFAULT_AMOUNT = '1';
 
@@ -69,8 +105,7 @@ const AddTodoItemForm = (props: RouteComponentProps) => {
 
     const [selectedProduct, setSelectedProduct] = useState<Product | null>();
     const [newItemQuantity, setNewItemQuantity] = useState(DEFAULT_AMOUNT);
-
-    const productSelectRef = useRef<any>();
+    const [productSearchInput, setProductSearchInput] = useState('');
 
     const handleQuantityFieldChange = (quantity: number) => {
         setNewItemQuantity(quantity.toString());
@@ -78,18 +113,13 @@ const AddTodoItemForm = (props: RouteComponentProps) => {
 
     const handleSubmit = (e: any, context: TodoItemListContextType) => {
         e.preventDefault();
-        
+
         const quantity = parseInt(newItemQuantity);
         if (selectedProduct && quantity > 0) {
             const newItem = TodoItem.fromProduct(selectedProduct, quantity);
             context.addItem(newItem);
             setNewItemQuantity(DEFAULT_AMOUNT);
-
-            if (productSelectRef) {
-                // workaround to reset value on adding item (element was found by debug)
-                // console.log(productSelectRef);
-                productSelectRef.current.select.select.select.clearValue();
-            }
+            setProductSearchInput('');
         }
     }
 
@@ -113,9 +143,11 @@ const AddTodoItemForm = (props: RouteComponentProps) => {
                                 <TableRow>
                                     <TableCell align="left">
                                         {/* <TextField id="standard-basic" label="Add new" /> */}
-                                        <ProductSelect onProductSelect={onProductSelect}
-                                                       onProductCreateOptionSelect={onProductCreateOptionSelect} 
-                                                       ref={productSelectRef} />
+                                        <ProductSelect
+                                            inputValue={productSearchInput}
+                                            setInputValue={setProductSearchInput}
+                                            onProductSelect={onProductSelect}
+                                            onProductCreateOptionSelect={onProductCreateOptionSelect} />
                                     </TableCell>
                                     <TableCell align="left" padding="none">
                                         <QuantityField defaultQuantity={1} onChange={handleQuantityFieldChange} />
