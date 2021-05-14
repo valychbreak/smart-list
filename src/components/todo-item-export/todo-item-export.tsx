bcfe55fd8b4b0d831359db80373080e7f065b09d
Valerii Brylov <valychbreak@gmail.com>
2021-05-15 01:29:26 +0200

    [S12] Add export result view

--- src/components/todo-item-export/todo-item-export.tsx
+++ src/components/todo-item-export/todo-item-export.tsx
@@ -1,11 +1,14 @@
 import _ from "lodash";
-import { Button, Dialog } from "@material-ui/core";
-import { useEffect, useState } from "react";
+import { Box, Button, Dialog } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Alert } from "@material-ui/lab";
+import { ListAlt } from "@material-ui/icons";
 import ProductApi from "../../api/ProductApi";
 import TodoProductItemsApi from "../../api/TodoProductItemsApi";
 import ExportItem from "./export-item";
 import ExportItemEditForm, { ExportItemFormSubmitData } from "./export-item-form";
 import ExportItemList from "./export-item-list";
+import ExportResultView from "./export-result-view";
 
 export type ExportItemsGrouped = {
     categoryName: string;
@@ -34,15 +37,18 @@ export function getExportResult(exportItems: ExportItem[]): ExportItemsGrouped[]
 const TodoItemExport = () => {
     const [todoItems, setTodoItems] = useState<ExportItem[]>([]);
     const [openExportResultDialog, setOpenExportResultDialog] = useState<boolean>(false);
+    const exportItemsGrouped = openExportResultDialog ? getExportResult(todoItems) : [];
 
     const [selectedExportItem, setSelectedExportItem] = useState<ExportItem | null>(null);
     const openEditDialog = selectedExportItem !== null;
 
     useEffect(() => {
         TodoProductItemsApi.fetchTodoProductItems().then((loadedTodoItems) => {
-            const exportItems: ExportItem[] = loadedTodoItems.map((todoItem) => (
-                ExportItem.fromTodoItem(todoItem)
-            ));
+            const exportItems: ExportItem[] = loadedTodoItems
+                .filter((todoItem) => todoItem.isBought)
+                .map((todoItem) => (
+                    ExportItem.fromTodoItem(todoItem)
+                ));
 
             setTodoItems(exportItems);
         });
@@ -107,10 +113,18 @@ const TodoItemExport = () => {
                 open={openExportResultDialog}
                 onClose={() => setOpenExportResultDialog(false)}
             >
-                test
+                <ExportResultView exportItemsGrouped={exportItemsGrouped} />
             </Dialog>
 
-            <Button onClick={showExport}>Show export</Button>
+            <Alert severity="info">Showing only purchased items</Alert>
+            <Box marginY={1}>
+                <Button variant="outlined"
+                    startIcon={<ListAlt />}
+                    onClick={showExport}
+                >
+                    Show export
+                </Button>
+            </Box>
             <ExportItemList exportItems={todoItems} onEdit={onEditAction} />
         </>
     );
