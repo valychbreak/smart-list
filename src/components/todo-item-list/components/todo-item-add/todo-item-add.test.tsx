3336753c6fbd542116a2e38ef70fe81b7b836bb5
Valerii Brylov <valychbreak@gmail.com>
2021-06-08 23:26:19 +0200

    [S33] Replace alert with a product form in dialog

--- src/components/todo-item-list/components/todo-item-add/todo-item-add.test.tsx
+++ src/components/todo-item-list/components/todo-item-add/todo-item-add.test.tsx
@@ -1,26 +1,52 @@
 import { act } from "@testing-library/react";
 import { shallow } from "enzyme";
+import td from "testdouble";
 import React from "react";
 import Scanner from "../../../../Scanner";
-import AddTodoItemComponent from "./todo-item-add";
+import AddTodoItemComponent, { addProductToTodoItems } from "./todo-item-add";
 import TodoItemAddForm from "./todo-item-add-form";
+import Product from "../../../../entity/Product";
+import ProductApi from "../../../../api/ProductApi";
+import TodoItem from "../../types";
 
 describe("AddTodoItemComponent", () => {
     const addTodoItemComponent = (): JSX.Element => <AddTodoItemComponent />;
 
-    it("should display form by default", () => {
-        const wrapper = shallow(addTodoItemComponent());
+    describe("render", () => {
+        it("should display form by default", () => {
+            const wrapper = shallow(addTodoItemComponent());
 
-        expect(wrapper.containsMatchingElement(<TodoItemAddForm />)).toBe(true);
-    });
-
-    it("should open scanner on button click", () => {
-        const wrapper = shallow(addTodoItemComponent());
-
-        act(() => {
-            wrapper.find("[data-test-id='open-scanner-btn']").simulate("click");
+            expect(wrapper.find(TodoItemAddForm).exists())
+                .toBe(true);
         });
 
-        expect(wrapper.find(Scanner).exists()).toBe(true);
+        it("should open scanner on button click", () => {
+            const wrapper = shallow(addTodoItemComponent());
+
+            act(() => {
+                wrapper.find("[data-test-id='open-scanner-btn']").simulate("click");
+            });
+
+            expect(wrapper.find(Scanner).exists()).toBe(true);
+        });
+    });
+
+    describe("addProductToTodoItems", () => {
+        it("should add product to todo items", async () => {
+            // given
+            const product = td.object<Product>();
+            jest.spyOn(ProductApi, "saveProduct").mockResolvedValue(
+                product
+            );
+
+            type AddItem = (todoItem: TodoItem) => void;
+            const addItemMock = td.func<AddItem>();
+
+            // when
+            await addProductToTodoItems(product, addItemMock);
+
+            // then
+            td.verify(addItemMock(td.matchers.anything()));
+        });
     });
 });
--- src/components/todo-item-list/components/todo-item-add/todo-item-add.tsx
+++ src/components/todo-item-list/components/todo-item-add/todo-item-add.tsx
@@ -1,10 +1,22 @@
-import { Dialog, Fab, Grid } from "@material-ui/core";
+import { Dialog, DialogContent, DialogContentText, DialogTitle, Fab, Grid } from "@material-ui/core";
 import SettingsOverscanIcon from "@material-ui/icons/SettingsOverscan";
-import { useHistory } from "react-router-dom";
-import { useEffect } from "react";
 import Scanner from "../../../../Scanner";
 import TodoItemAddForm from "./todo-item-add-form";
 import useTodoItemAddController from "./todo-item-add-controller";
+import ProductForm from "../../../ProductForm";
+import Product from "../../../../entity/Product";
+import ProductApi from "../../../../api/ProductApi";
+import { useTodoItemListContext } from "../../../../pages/groceries-todo/context/TodoItemListContext";
+import TodoItem from "../../types";
+
+// export for testing only
+export async function addProductToTodoItems(
+    product: Product,
+    addItem: (todoItem: TodoItem) => void
+) {
+    const savedProduct = await ProductApi.saveProduct(product);
+    addItem(TodoItem.fromProduct(savedProduct));
+}
 
 const AddTodoItemComponent = () => {
     const {
@@ -15,24 +27,35 @@ const AddTodoItemComponent = () => {
         disableScanner,
         onBarcodeDetected,
         addTodoItem,
+        setOpenNewProductDialog,
     } = useTodoItemAddController();
 
-    const history = useHistory();
+    const { addItem } = useTodoItemListContext();
 
-    useEffect(() => {
-        if (!openNewProductDialog || lastBarcodeScanResult === null) {
-            return;
-        }
-
-        const { code, format } = lastBarcodeScanResult;
-        // eslint-disable-next-line no-alert
-        if (window.confirm(`Scanned barcode  ${code} (${format}) does not exist in our database. \nDo you want to go to 'Add new item' page?`)) {
-            history.push("new-product");
-        }
-    }, [openNewProductDialog]);
+    const onNewProductSubmit = async (product: Product) => {
+        addProductToTodoItems(product, addItem);
+        setOpenNewProductDialog(false);
+    };
 
     return (
         <>
+            <Dialog open={openNewProductDialog} onClose={() => setOpenNewProductDialog(false)}>
+                <DialogTitle>New product</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {/* eslint-disable-next-line max-len */}
+                        Scanned barcode {lastBarcodeScanResult?.code} ({lastBarcodeScanResult?.format}) does not exist in our database.
+                    </DialogContentText>
+                    <DialogContentText>
+                        Please, fill the following form to add a new product
+                    </DialogContentText>
+                    <ProductForm
+                        shortForm
+                        productBarcode={lastBarcodeScanResult?.code || ""}
+                        productBarcodeType={lastBarcodeScanResult?.format || ""}
+                        onProductSubmit={(product) => onNewProductSubmit(product)} />
+                </DialogContent>
+            </Dialog>
             <Grid container justify="center" alignItems="center">
                 <Grid item xs>
                     <Fab
