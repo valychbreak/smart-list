05626e3234fbd5371ee6eec180fe05c23bb53958
Valerii Brylov <valychbreak@gmail.com>
2021-07-10 22:24:27 +0200

    [S51] Handle external liks for product photos in new product form

--- src/api/ProductApi.tsx
+++ src/api/ProductApi.tsx
@@ -37,13 +37,16 @@ function getPaged(page: number, perPage: number, products: Product[]): Page<Prod
     );
 }
 
-class MockedProductApi implements ProductApi {
-    productCache: Product[];
-
-    constructor() {
-        this.productCache = [];
+async function getBase64ImageOrUrl(imageUrl: string): Promise<string> {
+    if (imageUrl.startsWith("http")) {
+        return imageUrl;
     }
 
+    const base64Image = await compressImage(imageUrl, "image/jpeg");
+    return base64Image;
+}
+
+class MockedProductApi implements ProductApi {
     async findGeneralNamesBy(query: string): Promise<string[]> {
         const matchingProducts = await LocalDB.findByGeneralNameOrFullName(query);
         return matchingProducts.map((product) => product.productGeneralName)
@@ -114,20 +117,18 @@ class MockedProductApi implements ProductApi {
         product.productCountry = country;
 
         if (imageUrl) {
-            const base64Image = await compressImage(imageUrl, "image/jpeg");
-            product.image = base64Image;
+            const base64ImageOrUrl = await getBase64ImageOrUrl(imageUrl);
+            product.image = base64ImageOrUrl;
         }
 
         return LocalDB.saveProduct(product);
     }
 
     async updateProduct(product: Product, productImage?: string): Promise<void> {
-        let base64Image: string;
-
         if (productImage) {
-            base64Image = await compressImage(productImage, "image/jpeg");
+            const base64ImageOrUrl = await getBase64ImageOrUrl(productImage);
             // eslint-disable-next-line no-param-reassign
-            product.image = base64Image;
+            product.image = base64ImageOrUrl;
         }
         await LocalDB.replaceProduct(product);
     }
